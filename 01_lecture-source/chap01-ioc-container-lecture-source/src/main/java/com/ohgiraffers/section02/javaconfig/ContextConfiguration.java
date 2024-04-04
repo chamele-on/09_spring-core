a01e6593eabd63b4c2fe5d38af044dbd1349c25c
chamele-on <skykitty13@naver.com>
2024-04-04 14:35:34 +0900

    ^^;

---
+++ 01_lecture-source/chap01-ioc-container-lecture-source/src/main/java/com/ohgiraffers/section02/javaconfig/ContextConfiguration.java
@@ -0,0 +1,19 @@
+package com.ohgiraffers.section02.javaconfig;
+
+import com.ohgiraffers.common.MemberDTO;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.beans.BeanProperty;
+@Configuration
+
+public class ContextConfiguration {
+
+    @Bean
+    public MemberDTO getMember() {
+
+        return new MemberDTO(1, "user01", "pass01", "조평훈");
+
+    }
+
+}
